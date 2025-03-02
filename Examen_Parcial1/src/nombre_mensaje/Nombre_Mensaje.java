fb4b75ae9b4581f37e40eb4d0affbd72c97b29ae
Emanuel Jimenez <ejimenezc17@miumg.edu.gt>
2025-03-02 16:13:59 -0600

    Add files via upload

---
+++ Examen_Parcial1/src/nombre_mensaje/Nombre_Mensaje.java
@@ -0,0 +1,11 @@
+package nombre_mensaje;
+
+public class Nombre_Mensaje {
+
+	public static void main(String[] args) {
+        System.out.println("Mi nombre es Emanuel.");
+        System.out.println("¡Estoy aprendiendo Java!");
+
+	}
+
+}
---
+++ Examen_Parcial1/src/variablesdiferentestipos/Variables_Diferentes.java
@@ -0,0 +1,18 @@
+package variablesdiferentestipos;
+
+public class Variables_Diferentes {
+
+	public static void main(String[] args) {
+        // Declaración de variables
+        int edad = 21;
+        double altura = 1.67;
+        String nombre = "Emanuel";
+
+        // Imprimir las variables
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+
+	}
+
+}
